0ae458bf43923366997e79cfc1847b89871f3792
Leonardo Maciel <leomaciel1990@hotmail.com>
2021-09-10 11:52:31 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/leonardomaciel1990" target="_blank" rel="noreferrer">Leonardo Maciel</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/leonardo.maciel.1990" target="_blank" rel="noreferrer">@leonardo.maciel.1990</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
