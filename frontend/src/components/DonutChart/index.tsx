d6117ae17eb6ae7fc508b228a4b92d1572aac446
Leonardo Maciel <leomaciel1990@hotmail.com>
2021-09-26 19:15:14 -0300

    Donut chart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
@@ -10,25 +11,42 @@ type ChartData = {
 
 const DonutChart = () => {
 
-     // Forma errada   
-    let chartData : ChartData = {labels:[], series:[]};
 
-    // Forma errada
-    axios.get(BASE_URL+'/sales/amount-by-seller')
-        .then( response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-            chartData = {labels:myLabels, series:mySeries};
-            console.log(chartData);
-        });
+    useEffect(() => {
+        axios.get(BASE_URL + '/sales/amount-by-seller')
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
-/*     const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    } */
+    // Forma errada   
+    //let chartData : ChartData = {labels:[], series:[]};
+
+    // Forma errada. Por ser requisições assíncronas  a variável chartData mesmo sendo carregada
+    // aqui dentro o componente não é renderizado. (Ciclo de vida do react) é necessário usar o useStates
+    /*     axios.get(BASE_URL+'/sales/amount-by-seller')
+            .then( response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
     
+                // Forma errada sem useState
+                //chartData = {labels:myLabels, series:mySeries};
+                //console.log(chartData);
+    
+                setChartData({labels:myLabels, series:mySeries});
+            }); */
+
+    /*     const mockData = {
+            series: [477138, 499928, 444867, 220426, 473088],
+            labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+        } */
+
     const options = {
         legend: {
             show: true
@@ -36,8 +54,8 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{ ...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
